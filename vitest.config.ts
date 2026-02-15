d602f52d381f2fef8a4cbacffbae8e2715bc82a2
Eric Brooks Martin <ericbmartin69@gmail.com>
2026-02-15 14:57:24 -0800

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,13 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  // This MUST match your repository name exactly
-  base: "/ericbrooksmartin.io/", 
-  
+  base: "/ericbrooksmartin.io/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+  }
 });
